83b52b86b75a3b32cf29731d04e4c14fdaf73e5c
xulinlin <18351942808@163.com>
2019-09-16 18:58:22 +0800

    menulist

--- src/views/Goods/Goods.vue
+++ src/views/Goods/Goods.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="goods-wrapper"></div>
+  <div class="goods-wrapper">
+    <menu-list class="menu-list"></menu-list>
+    <div class="right-list"></div>
+  </div>
 </template>
 
 <script>
-
+import menuList from './components/MenuList/MenuList'
 export default {
   name: 'goods',
   components: {
+    menuList
   }
 }
 </script>
 
 <style lang="less" scoped>
 .goods-wrapper {
+  position: absolute;
   background-color: red;
   width: 100%;
   height: 450px;
+
+  .menu-list {
+    position: absolute;
+    width: 60px;
+    bottom: 0;
+    top: 0;
+    left: 0;
+  }
+
+  .right-list {
+    position: absolute;
+    background-color: aqua;
+    bottom: 0;
+    top: 0;
+    left: 60px;
+    right: 0;
+  }
 }
 </style>
---
+++ src/views/Goods/components/MenuList/MenuList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-list-wrapper">
+    <ul class="ul-box">
+      <li>哈哈哈</li>
+      <li>哈哈哈</li>
+      <li>哈哈哈</li>
+      <li>哈哈哈</li>
+      <li>哈哈哈</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MenuList',
+  components: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.menu-list-wrapper {
+  width: 100%;
+
+  .ul-box {
+  }
+}
+</style>
